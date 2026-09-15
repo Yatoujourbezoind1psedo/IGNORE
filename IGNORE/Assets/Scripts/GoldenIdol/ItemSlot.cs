7a4f2bcab5819db7873746b5ffb973f57f912be0
Yatoujourbezoind1psedo <bonath03@gmail.com>
2026-09-15 17:01:09 +0200

    Mise en place de la reconnaissance du texte d'un enfant placé dans le slot

--- IGNORE/Assets/Scripts/GoldenIdol/ItemSlot.cs
+++ IGNORE/Assets/Scripts/GoldenIdol/ItemSlot.cs
@@ -1,17 +1,34 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class ItemSlot : MonoBehaviour, IDropHandler
 {
+    [SerializeField] private string nomAttendu;
     public void OnDrop(PointerEventData eventData)
     {
-        Debug.Log("OnDrop"); 
-        //eventData.pointerDrag = objet qui est drop
+        //Debug.Log("OnDrop"); 
+        //eventData.pointerDrag = objet qui est drop (objet qui reçoit OnDrag)
 
-        //Pour le mettre au centre de l'item slot
+        
         if(eventData.pointerDrag != null)
         {
-            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            //Pour le mettre au centre de l'item slot
+            eventData.pointerDrag.GetComponent<RectTransform>().position = GetComponent<RectTransform>().position;
+
+            Transform child = eventData.pointerDrag.transform.GetChild(0); //Récupère le premier enfant qui est le texte
+
+            //Debug.Log(child.GetComponent<TextMeshProUGUI>().text);
+
+            if(child.GetComponent<TextMeshProUGUI>().text.ToLower() == nomAttendu.ToLower())
+            {
+                Debug.Log(child.GetComponent<TextMeshProUGUI>().text); 
+            }
+
+            
+
+            
+        
         }
     }   
 }
